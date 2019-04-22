5c373852e85cd1696f9347e15abc4e923f402018
Farhandims <mfarhandimas@gmail.com>
2019-04-22 11:11:38 +0700

    adding add operation

---
+++ src/aritmatika/Aritmatika.java
@@ -0,0 +1,19 @@
+package aritmatika;
+
+public class Aritmatika {
+    
+    private double first ,second;
+    
+    public double add (double first , double second)
+    {
+    return first + second;    
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
